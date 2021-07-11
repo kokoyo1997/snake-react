8c3b6030f711c76de01859bd362ee454346cc5b8
kokoyo1997 <15071367672@163.com>
2021-07-11 20:09:50 +0800

    Updates

--- static/js/main.4e698229.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpsnake-react"]=this["webpackJsonpsnake-react"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);n(1);var r=n(3),c=n.n(r),s=(n(8),n(0));var a=function(){return Object(s.jsx)(s.Fragment,{})};c.a.render(Object(s.jsx)(a,{}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.4e698229.chunk.js.map
